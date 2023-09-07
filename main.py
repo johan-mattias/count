0212e53be531b9e95283edfb501847752b9af437
Johan <3921204+johan-mattias@users.noreply.github.com>
2023-09-07 16:27:11 +0200

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+
+basic.show_number(0)
+basic.show_number(1)
+basic.show_number(2)
--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-basic.forever(function () {
-    basic.showNumber(1)
-    basic.showNumber(2)
-    basic.showNumber(3)
-})
+basic.showNumber(0)
+basic.showNumber(1)
+basic.showNumber(2)
